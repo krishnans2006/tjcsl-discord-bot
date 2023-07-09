8540cf785d8f30d899f4febe3d489430276ee3fc
Krishnan Shankar <krishnans2006@gmail.com>
2023-07-08 21:20:15 -0400

    Basic Discord embeds

--- api.py
+++ api.py
@@ -18,12 +18,19 @@ def call_api(endpoint: str, **kwargs) -> dict:
     return response.json()
 
 
-def list_incidents(duration: timedelta = timedelta(minutes=1)) -> Iterator[Incident] | None:
+def list_incidents(
+    duration: timedelta = timedelta(minutes=1),
+) -> Iterator[tuple[Incident, datetime, bool]] | None:
     current_incidents = call_api("incidents")["data"]
     for incident in current_incidents:
         start_time = datetime.strptime(
             incident["attributes"]["started_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
         )
         if start_time > datetime.now() - duration:
-            yield incident
-    yield current_incidents[0]
+            yield incident, start_time, False
+        resolve_time = datetime.strptime(
+            incident["attributes"]["resolved_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+        if resolve_time > datetime.now() - duration:
+            yield incident, resolve_time, True
+    yield current_incidents[0], datetime.now(), False
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
 
+guild_id = 1037482630838489179
+channel_id = 1127402489013076038
+
 
 @client.event
 async def on_ready():
@@ -21,8 +24,30 @@ async def on_ready():
 @tasks.loop(minutes=1)
 async def check_incidents():
     new_incidents = list_incidents(duration=timedelta(minutes=1))
-    for new_incident in new_incidents:
-        print(new_incident)
+    channel = None
+    for incident, timestamp, is_resolved in new_incidents:
+        if not channel:
+            channel = client.get_guild(guild_id).get_channel(channel_id)
+        if is_resolved:
+            embed = discord.Embed(
+                title="Resolved Incident",
+                color=discord.Color.green(),
+                timestamp=timestamp,
+            )
+        else:
+            embed = discord.Embed(
+                title="New Incident",
+                color=discord.Color.red(),
+                timestamp=timestamp,
+            )
+        embed.add_field(name="For", value=incident["attributes"]["name"], inline=True)
+        embed.set_author(
+            name="Better Stack",
+            url="https://uptime.betterstack.com/team/58077/monitors",
+            icon_url="https://pbs.twimg.com/profile_images/1380564657505718275/TToZVzli_400x400.jpg",
+        )
+
+        await channel.send(embed=embed)
     print("Checked for new incidents")
 
 
