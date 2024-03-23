0382af0a7681f9be350b4dcbcc06be08a6242209
Krishnan Shankar <krishnans2006@gmail.com>
2024-03-23 14:32:20 -0400

    Use datetime.fromisoformat to account for daylight savings, improve type hinting
    Fixed ValueError: time data '...+01:00' does not match format '...Z'

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import requests
 
 BASE_URL = "https://uptime.betterstack.com/api/v2/"
 Incident = dict[str, str | dict[str, str]]
+IncidentList = Iterator[tuple[Incident, datetime, datetime | None, bool]]
 
 
 def call_api(endpoint: str, **kwargs) -> dict:
@@ -18,21 +19,15 @@ def call_api(endpoint: str, **kwargs) -> dict:
     return response.json()
 
 
-def list_incidents(
-    duration: timedelta = timedelta(minutes=1),
-) -> Iterator[tuple[Incident, datetime, datetime | None, bool]] | None:
+def list_incidents(duration: timedelta = timedelta(minutes=1)) -> IncidentList | None:
     current_incidents = call_api("incidents")["data"]
     for incident in current_incidents:
-        start_time = datetime.strptime(
-            incident["attributes"]["started_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
-        ).replace(tzinfo=timezone.utc)
+        start_time = datetime.fromisoformat(incident["attributes"]["started_at"])
         if start_time > datetime.now(tz=timezone.utc) - duration:
             yield incident, start_time, None, False
 
         if incident["attributes"]["resolved_at"] is None:
             continue
-        resolve_time = datetime.strptime(
-            incident["attributes"]["resolved_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
-        ).replace(tzinfo=timezone.utc)
+        resolve_time = datetime.fromisoformat(incident["attributes"]["resolved_at"])
         if resolve_time > datetime.now(tz=timezone.utc) - duration:
             yield incident, start_time, resolve_time, True
